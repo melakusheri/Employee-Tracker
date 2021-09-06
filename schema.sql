dc2d84e78d2ac8a65aa1491d56e3df468b3f3a10
Melaku <mailangels@gmail.com>
2021-09-06 18:36:06 -0400

    dependencies

--- schema.sql
+++ schema.sql
@@ -5,20 +5,20 @@ CREATE DATABASE tracker_db;
 USE  tracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (30) NOT NULL,
     PRIMARY KEY (id)
 
 );
 CREATE TABLE role (
-    id INT(10) NOT NULL,
+    id INT(10) NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT(10) NOT NULL,
     PRIMARY key (id)
 );
 CREATE TABLE employee (
-    id INT(10) NOT NULL,
+    id INT(10) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10) NOT NULL,
